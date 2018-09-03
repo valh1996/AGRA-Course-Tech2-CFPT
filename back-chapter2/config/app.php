bf93195ccdba8fc16a646ba2a3149b35fd8c8fb2
Valentin Hutter <valentinhutter@MacBook-Pro-de-Valentin.local>
2018-09-03 08:57:39 +0200

    add intervention/image to manipulate image files

--- back-chapter2/config/app.php
+++ back-chapter2/config/app.php
@@ -150,6 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -209,6 +210,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        /** custom */
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
